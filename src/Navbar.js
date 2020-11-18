7f851d77fa8d7603154206caa743e0f4934a6213
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-18 14:20:00 +0200

    added structure to our navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+
+import { useGlobalContext } from './context';
+import logo from './images/logo.svg';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  // fetching functions from context
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn' title="Products">
+              products
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' title="Developers">
+              developers
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' title="Company">
+              company
+            </button>
+          </li>
+        </ul>
+        <button className='btn signin-btn' title="Sign in">Sign in</button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
